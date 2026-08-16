18db86adcdd35cfacbb74594e97216515d8da859
prathwik-shetty <prathwikshetty843@gmail.com>
2026-08-17 00:45:06 +0530

    Configure Firebase Admin for deployment

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
-const serviceAccount = require("./auditvault-25052-firebase-adminsdk-fbsvc-00003e47b7.json");
+const serviceAccount = require("/etc/secrets/auditvault-25052-firebase-adminsdk-fbsvc-00003e47b7.json");
 
 initializeApp({
   credential: cert(serviceAccount),
